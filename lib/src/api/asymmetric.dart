8c39268430e48e08943c1d9e92a4a324089bc745
Ivan Zaera Avellon <izaera@gmail.com>
2014-01-10 16:13:33 +0100

    Split api.dart in parts to make it more manageable

---
+++ lib/src/api/asymmetric.dart
@@ -0,0 +1,93 @@
+// Copyright (c) 2013, Iván Zaera Avellón - izaera@gmail.com
+// Use of this source code is governed by a LGPL v3 license.
+// See the LICENSE file for more information.
+
+part of cipher.api;
+
+/// The interface that asymmetric (public and private) keys conform to.
+abstract class AsymmetricKey {
+}
+
+/// The interface that asymmetric public keys conform to.
+abstract class PublicKey implements AsymmetricKey {
+}
+
+/// The interface that asymmetric private keys conform to.
+abstract class PrivateKey implements AsymmetricKey {
+}
+
+/// A pair of public and private asymmetric keys.
+class AsymmetricKeyPair {
+
+  final PublicKey publicKey;
+  final PrivateKey privateKey;
+
+  AsymmetricKeyPair(this.publicKey, this.privateKey);
+
+}
+
+/// A signature created by a [Signer]
+class Signature {
+
+  final BigInteger r;
+  final BigInteger s;
+
+  Signature( this.r, this.s );
+
+  String toString() => "(${r.toString()},${s.toString()})";
+
+}
+
+/// An interface for DSAs (digital signature algorithms)
+abstract class Signer {
+
+  /// The [Registry] for [Signer] algorithms
+  static final registry = new Registry<Signer>();
+
+  /// Create the signer specified by the standard [algorithmName].
+  factory Signer( String algorithmName ) => registry.create(algorithmName);
+
+  /// Get this signer's standard algorithm name.
+  String get algorithmName;
+
+  /// Reset the signer to its original state.
+  void reset();
+
+  /**
+   * Init the signer with its initialization [params]. The type of [CipherParameters] depends on the algorithm being used (see
+   * the documentation of each implementation to find out more).
+   *
+   * Use the argument [forSigning] to tell the signer if you want to generate or verify signatures.
+   */
+  void init( bool forSigning, CipherParameters params );
+
+  /// Sign the passed in [message] (usually the output of a hash function)
+  Signature generateSignature( Uint8List message );
+
+  /// Verify the [message] against the [signature].
+  bool verifySignature( Uint8List message, Signature signature );
+
+}
+
+/// The interface that asymmetric key generators conform to.
+abstract class KeyGenerator {
+
+  /// The [Registry] for [KeyGenerator] algorithms
+  static final registry = new Registry<KeyGenerator>();
+
+  /// Create the key generator specified by the standard [algorithmName].
+  factory KeyGenerator( String algorithmName ) => registry.create(algorithmName);
+
+  /// Get this generator's standard algorithm name.
+  String get algorithmName;
+
+  /**
+   * Init the generator with its initialization [params]. The type of [CipherParameters] depends on the algorithm being used
+   * (see the documentation of each implementation to find out more).
+   */
+  void init( CipherParameters params );
+
+  /// Derive key from given input and put it in [out] at offset [outOff].
+  AsymmetricKeyPair generateKeyPair();
+
+}
---
+++ lib/src/api/unkeyed.dart
@@ -0,0 +1,73 @@
+// Copyright (c) 2013, Iván Zaera Avellón - izaera@gmail.com
+// Use of this source code is governed by a LGPL v3 license.
+// See the LICENSE file for more information.
+
+part of cipher.api;
+
+/// The interface that a message digest conforms to.
+abstract class Digest {
+
+  /// The [Registry] for [Digest] algorithms
+  static final registry = new Registry<Digest>();
+
+  /// Create the digest specified by the standard [algorithmName].
+  factory Digest( String algorithmName ) => registry.create(algorithmName);
+
+  /// Get this digest's standard algorithm name.
+  String get algorithmName;
+
+  /// Get this digest's output size.
+  int get digestSize;
+
+  /// Reset the digest to its original state.
+  void reset();
+
+  /// Add one byte of data to the digested input.
+  void updateByte( int inp );
+
+  /**
+   * Add [len] bytes of data contained in [inp], starting at position [inpOff]
+   * ti the digested input.
+   */
+  void update( Uint8List inp, int inpOff, int len );
+
+  /**
+   * Store the digest of previously given data in buffer [out] starting at
+   * offset [outOff]. This method returns the size of the digest.
+   */
+  int doFinal( Uint8List out, int outOff );
+
+}
+
+/// The interface that a padding conforms to.
+abstract class Padding {
+
+  /// The [Registry] for [Padding] algorithms
+  static final registry = new Registry<Padding>();
+
+  /// Create the digest specified by the standard [algorithmName].
+  factory Padding( String algorithmName ) => registry.create(algorithmName);
+
+  /// Get this padding's standard algorithm name.
+  String get algorithmName;
+
+  /// Initialise the padder. Normally, paddings don't need any init params.
+  void init( [CipherParameters params] );
+
+  /**
+   * Add the pad bytes to the passed in block, returning the number of bytes
+   * added.
+   *
+   * Note: this assumes that the last block of plain text is always passed to it
+   * inside [data]. i.e. if [offset] is zero, indicating the entire block is to
+   * be overwritten with padding the value of [data] should be the same as the
+   * last block of plain text. The reason for this is that some modes such as
+   * "trailing bit compliment" base the padding on the last byte of plain text.
+   */
+  int addPadding( Uint8List data, int offset );
+
+  /// Get the number of pad bytes present in the block.
+  int padCount( Uint8List data );
+
+}
+
