364742a6bc893f9d85cd7861ad05e1ff193dbe49
Ivan Zaera Avellon <izaera@gmail.com>
2014-02-04 12:22:42 +0100

    Add MD5 to registry test

--- test/src/registry_test.dart
+++ test/src/registry_test.dart
@@ -33,6 +33,7 @@ void main() {
 
       testDigest( "MD2" );
       testDigest( "MD4" );
+      testDigest( "MD5" );
       testDigest( "RIPEMD-160" );
       testDigest( "SHA-1" );
       testDigest( "SHA-224" );
