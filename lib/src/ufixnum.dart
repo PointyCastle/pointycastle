a0953927a5d46f65ee3bc1a58ce322a7f156d741
Ivan Zaera Avellon <izaera@gmail.com>
2014-02-05 13:51:45 +0100

    Accept UintX's as argument to clip and coerce

--- lib/src/ufixnum.dart
+++ lib/src/ufixnum.dart
@@ -7,24 +7,24 @@ part of cipher.api;
 /// Implementation of unsigned 8-bit size nums
 class Uint8 extends UintX {
 
-  static int clip( int value ) => (value&0xFF);
+  static int clip(value) => (value.toInt()&0xFF);
 
-  Uint8(int value) : super(value);
+  Uint8(value) : super(value.toInt());
 
   int get bitLength => 8;
   int get byteLength => 1;
 
-  int _clip( int value ) => clip(value);
-  Uint8 _coerce(int value) => new Uint8(value);
+  int _clip(value) => clip(value);
+  Uint8 _coerce(value) => new Uint8(value);
 
 }
 
 /// Implementation of unsigned 16-bit size nums
 class Uint16 extends UintX {
 
-  static int clip( int value ) => (value&0xFFFF);
+  static int clip(value) => (value.toInt()&0xFFFF);
 
-  Uint16(int value) : super(value);
+  Uint16(value) : super(value.toInt());
 
   Uint16.fromBigEndian( Uint8List value, int offset ) :
     super( new ByteData.view(value.buffer).getUint16(offset, Endianness.BIG_ENDIAN ) );
@@ -35,17 +35,17 @@ class Uint16 extends UintX {
   int get bitLength => 16;
   int get byteLength => 2;
 
-  int _clip( int value ) => clip(value);
-  Uint16 _coerce(int value) => new Uint16(value);
+  int _clip(value) => clip(value);
+  Uint16 _coerce(value) => new Uint16(value);
 
 }
 
 /// Implementation of unsigned 32-bit size nums
 class Uint32 extends UintX {
 
-  static int clip( int value ) => (value&0xFFFFFFFF);
+  static int clip(value) => (value.toInt()&0xFFFFFFFF);
 
-  Uint32(int value) : super(value);
+  Uint32(value) : super(value.toInt());
 
   Uint32.fromBigEndian( Uint8List value, int offset ) :
     super( new ByteData.view(value.buffer).getUint32(offset, Endianness.BIG_ENDIAN ) );
@@ -56,17 +56,17 @@ class Uint32 extends UintX {
   int get bitLength => 32;
   int get byteLength => 4;
 
-  int _clip( int value ) => clip(value);
-  Uint32 _coerce(int value) => new Uint32(value);
+  int _clip(value) => clip(value);
+  Uint32 _coerce(value) => new Uint32(value);
 
 }
 
 /// Implementation of unsigned 64-bit size nums
 class Uint64 extends UintX {
 
-  static int clip( int value ) => (value&0xFFFFFFFFFFFFFFFF);
+  static int clip(value) => (value.toInt()&0xFFFFFFFFFFFFFFFF);
 
-  Uint64(int value) : super(value);
+  Uint64(value) : super(value.toInt());
 
   Uint64.fromBigEndian( Uint8List value, int offset ) :
     super( (new Uint32.fromBigEndian(value, offset).toInt() << 32) | new Uint32.fromBigEndian(value, offset+4).toInt() );
@@ -77,8 +77,8 @@ class Uint64 extends UintX {
   int get bitLength => 64;
   int get byteLength => 8;
 
-  int _clip( int value ) => clip(value);
-  Uint64 _coerce(int value) => new Uint64(value);
+  int _clip(value) => clip(value);
+  Uint64 _coerce(value) => new Uint64(value);
 
 }
 
@@ -88,7 +88,7 @@ abstract class UintX {
   int _value;
 
   /// Create an [UintX] from a given [value]. The value can be clipped if it cannot fit into this [UintX].
-  UintX( int value ) {
+  UintX(value) {
     _value = _clip(value);
   }
 
@@ -98,8 +98,8 @@ abstract class UintX {
   /// Get the byte length of this integer
   int get byteLength;
 
-  int _clip( int value );
-  UintX _coerce( int value );
+  int _clip(value);
+  UintX _coerce(value);
 
   /// Convert to a regular [int].
   int toInt() => _value;
@@ -128,14 +128,14 @@ abstract class UintX {
   UintX operator >>(n) => _coerce( _value>>(n.toInt()%bitLength) );
 
   /// Circular shift left
-  UintX rotl( int n ) {
+  UintX rotl(int n) {
     if( n<0 ) throw new ArgumentError("Shift offset cannot be negative");
     n = (n%bitLength);
     return _coerce( _clip(_value << n) | _clip(_value >> (bitLength - n)) );
   }
 
   /// Circular shift right
-  UintX rotr( int n ) {
+  UintX rotr(int n) {
     if( n<0 ) throw new ArgumentError("Shift offset cannot be negative");
     n = (n%bitLength);
     return _coerce( _clip(_value >> n) | _clip(_value << (bitLength - n)) );
@@ -164,7 +164,7 @@ abstract class UintX {
   String toString() => toInt().toString();
   String toRadixString( int radix ) => toInt().toRadixString(radix);
 
-  int _int( value ) {
+  int _int(value) {
     if( value is int ) {
       return value;
     } else if( value.runtimeType==runtimeType ) {
