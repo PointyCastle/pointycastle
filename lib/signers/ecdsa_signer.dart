901cf303000b717f95ecf454ab2d1184efbc59d9
Steven Roose <stevenroose@gmail.com>
2014-03-03 22:43:09 +0100

    Removed some unused code

--- lib/signers/ecdsa_signer.dart
+++ lib/signers/ecdsa_signer.dart
@@ -353,15 +353,3 @@ class _RandomKCalculator {
   }
 
 }
-
-
-
-
-String formatBytesAsHexString(Uint8List bytes) {
-  var result = new StringBuffer();
-  for( var i=0 ; i<bytes.lengthInBytes ; i++ ) {
-    var part = bytes[i];
-    result.write('${part < 16 ? '0' : ''}${part.toRadixString(16)}');
-  }
-  return result.toString();
-}
