d65fd4cf49b96019b5273316763e3f2d492d8cf5
Ivan Zaera Avellon <izaera@gmail.com>
2014-03-20 07:29:52 +0100

    #64 Optimize Salsa20

---
+++ benchmark/benchmark/stream_cipher_benchmark.dart
@@ -0,0 +1,51 @@
+// Copyright (c) 2013, Iván Zaera Avellón - izaera@gmail.com
+// Use of this source code is governed by a LGPL v3 license.
+// See the LICENSE file for more information.
+
+library cipher.benchmark.benchmark.stream_cipher_benchmark;
+
+import "dart:typed_data";
+
+import "package:cipher/cipher.dart";
+import "package:cipher/impl/server.dart";
+import "../benchmark/rate_benchmark.dart";
+
+typedef CipherParameters CipherParametersFactory();
+
+class StreamCipherBenchmark extends RateBenchmark {
+
+  final String _streamCipherName;
+  final bool _forEncryption;
+  final CipherParametersFactory _cipherParametersFactory;
+  final Uint8List _data;
+
+  StreamCipher _streamCipher;
+
+  StreamCipherBenchmark(String streamCipherName, String streamCipherVariant, bool forEncryption,
+      this._cipherParametersFactory, [int dataLength = 1024*1024]) :
+    super("StreamCipher | $streamCipherName ${_formatVariant(streamCipherVariant)}- "
+        "${forEncryption ? 'encrypt' : 'decrypt' }"),
+    _streamCipherName = streamCipherName,
+    _forEncryption = forEncryption,
+    _data = new Uint8List(dataLength);
+
+  void setup() {
+    initCipher();
+    _streamCipher = new StreamCipher(_streamCipherName);
+    _streamCipher.init(_forEncryption, _cipherParametersFactory());
+  }
+
+  void run() {
+    _streamCipher.process(_data);
+    addSample(_data.length);
+  }
+
+}
+
+String _formatVariant(String streamCipherVariant) {
+  if (streamCipherVariant == null) {
+    return "";
+  } else {
+    return "- $streamCipherVariant ";
+  }
+}
--- lib/stream/salsa20.dart
+++ lib/stream/salsa20.dart
@@ -14,25 +14,23 @@ import "package:cipher/stream/base_stream_cipher.dart";
 /// Implementation of Daniel J. Bernstein's Salsa20 stream cipher, Snuffle 2005.
 class Salsa20Engine extends BaseStreamCipher {
 
-  static const _STATE_SIZE = 16; // 16, 32 bit ints = 64 bytes
-  static const _BYTE_LIMIT = 0x400000000000000000;
+  static const _STATE_SIZE = 16;
 
   static final _sigma = new Uint8List.fromList( "expand 32-byte k".codeUnits );
   static final _tau = new Uint8List.fromList( "expand 16-byte k".codeUnits );
 
-  // Variables to hold the state of the engine during encryption and decryption
-  var _index = 0;
-  var _engineState = new List<int>(_STATE_SIZE); // state
-  var _x = new List<int>(_STATE_SIZE); // internal buffer
-  var _keyStream  = new Uint8List(_STATE_SIZE * 4); // expanded state, 64 bytes
   Uint8List _workingKey  = null;
   Uint8List _workingIV   = null;
+
+  final _state = new List<int>(_STATE_SIZE);
+  final _buffer = new List<int>(_STATE_SIZE);
+
+  final _keyStream  = new Uint8List(_STATE_SIZE * 4);
+  var _keyStreamOffset = 0;
+
   var _initialised = false;
 
-  // Internal counter for detecting algorithm limit overflow
-  int _counter;
-
-  String get algorithmName => "Salsa20";
+  final String algorithmName = "Salsa20";
 
   void reset() {
     if( _workingKey!=null ) {
@@ -41,8 +39,6 @@ class Salsa20Engine extends BaseStreamCipher {
   }
 
   void init( bool forEncryption, ParametersWithIV<KeyParameter> params ) {
-    // Salsa20 encryption and decryption is completely symmetrical, so the 'forEncryption' is irrelevant.
-
     var uparams = params.parameters;
     var iv = params.iv;
     if( iv == null || iv.length != 8 ) {
@@ -56,23 +52,16 @@ class Salsa20Engine extends BaseStreamCipher {
   }
 
   int returnByte( int inp ) {
-    if( _limitExceeded() ) {
-      throw new StateError(
-        "Salsa20 can only securely cipher 2^70 bytes per IV: please change IV in order to stay secure and not incur in a "
-        "two-time pad type error"
-      );
-    }
-
-    if (_index == 0) {
+    if (_keyStreamOffset == 0) {
       _generateKeyStream(_keyStream);
 
-      if (++_engineState[8] == 0) {
-        ++_engineState[9];
+      if (++_state[8] == 0) {
+        ++_state[9];
       }
     }
 
-    var out = (_keyStream[_index]^inp)&0xFF;
-    _index = (_index + 1) & 63;
+    var out = clip8(_keyStream[_keyStreamOffset] ^ inp);
+    _keyStreamOffset = (_keyStreamOffset + 1) & 63;
 
     return out;
   }
@@ -90,24 +79,17 @@ class Salsa20Engine extends BaseStreamCipher {
       throw new ArgumentError( "Output buffer too short or requested length too long" );
     }
 
-    if( _limitExceeded(len) ) {
-      throw new ArgumentError(
-        "Salsa20 can only securely cipher 2^70 bytes per IV, requested length too long: call processBytes() several times "
-        "changing IV between calls"
-      );
-    }
-
     for( var i=0 ; i<len ; i++ ) {
-      if( _index==0 ) {
+      if( _keyStreamOffset==0 ) {
         _generateKeyStream(_keyStream);
 
-        if( ++_engineState[8] == 0 ) {
-          ++_engineState[9];
+        if( ++_state[8] == 0 ) {
+          ++_state[9];
         }
       }
 
-      out[i+outOff] = (_keyStream[_index]^inp[i+inpOff])&0xFF;
-      _index = (_index + 1) & 63;
+      out[i+outOff] = clip8(_keyStream[_keyStreamOffset] ^ inp[i + inpOff]);
+      _keyStreamOffset = (_keyStreamOffset + 1) & 63;
     }
   }
 
@@ -116,58 +98,47 @@ class Salsa20Engine extends BaseStreamCipher {
     _workingKey = keyBytes;
     _workingIV  = ivBytes;
 
-    _index = 0;
-    _resetCounter();
+    _keyStreamOffset = 0;
     int offset = 0;
     Uint8List constants;
 
     // Key
-    _engineState[1] = new Uint32.fromLittleEndian(_workingKey,0).toInt();
-    _engineState[2] = new Uint32.fromLittleEndian(_workingKey,4).toInt();
-    _engineState[3] = new Uint32.fromLittleEndian(_workingKey,8).toInt();
-    _engineState[4] = new Uint32.fromLittleEndian(_workingKey,12).toInt();
+    _state[1] = unpack32(_workingKey,  0, Endianness.LITTLE_ENDIAN);
+    _state[2] = unpack32(_workingKey,  4, Endianness.LITTLE_ENDIAN);
+    _state[3] = unpack32(_workingKey,  8, Endianness.LITTLE_ENDIAN);
+    _state[4] = unpack32(_workingKey, 12, Endianness.LITTLE_ENDIAN);
 
     if( _workingKey.length == 32 ) {
-        constants = _sigma;
-        offset = 16;
+      constants = _sigma;
+      offset = 16;
     } else {
-        constants = _tau;
+      constants = _tau;
     }
 
-    _engineState[11] = new Uint32.fromLittleEndian(_workingKey,offset).toInt();
-    _engineState[12] = new Uint32.fromLittleEndian(_workingKey,offset+4).toInt();
-    _engineState[13] = new Uint32.fromLittleEndian(_workingKey,offset+8).toInt();
-    _engineState[14] = new Uint32.fromLittleEndian(_workingKey,offset+12).toInt();
-    _engineState[0 ] = new Uint32.fromLittleEndian(constants,0).toInt();
-    _engineState[5 ] = new Uint32.fromLittleEndian(constants,4).toInt();
-    _engineState[10] = new Uint32.fromLittleEndian(constants,8).toInt();
-    _engineState[15] = new Uint32.fromLittleEndian(constants,12).toInt();
+    _state[11] = unpack32(_workingKey, offset     , Endianness.LITTLE_ENDIAN);
+    _state[12] = unpack32(_workingKey, offset +  4, Endianness.LITTLE_ENDIAN);
+    _state[13] = unpack32(_workingKey, offset +  8, Endianness.LITTLE_ENDIAN);
+    _state[14] = unpack32(_workingKey, offset + 12, Endianness.LITTLE_ENDIAN);
+    _state[0 ] = unpack32(constants,  0, Endianness.LITTLE_ENDIAN);
+    _state[5 ] = unpack32(constants,  4, Endianness.LITTLE_ENDIAN);
+    _state[10] = unpack32(constants,  8, Endianness.LITTLE_ENDIAN);
+    _state[15] = unpack32(constants, 12, Endianness.LITTLE_ENDIAN);
 
     // IV
-    _engineState[6] = new Uint32.fromLittleEndian(_workingIV,0).toInt();
-    _engineState[7] = new Uint32.fromLittleEndian(_workingIV,4).toInt();
-    _engineState[8] = _engineState[9] = 0;
+    _state[6] = unpack32(_workingIV, 0, Endianness.LITTLE_ENDIAN);
+    _state[7] = unpack32(_workingIV, 4, Endianness.LITTLE_ENDIAN);
+    _state[8] = _state[9] = 0;
 
     _initialised = true;
   }
 
   void _generateKeyStream( Uint8List output ) {
-    _salsa20Core(20, _engineState, _x);
+    _salsa20Core(20, _state, _buffer);
     var outOff = 0;
-    for( var x in _x ) {
-      new Uint32(x).toLittleEndian(output, outOff);
+    for(var x in _buffer) {
+      pack32(x, output, outOff, Endianness.LITTLE_ENDIAN);
       outOff += 4;
     }
-  }
-
-  void _resetCounter() {
-    _counter = 0;
-  }
-
-  bool _limitExceeded([int len=1]) {
-    if( len<0 ) throw new ArgumentError("Length is supposed to be >0");
-    _counter += len;
-    return _counter==_BYTE_LIMIT;
   }
 
   /// The Salsa20 core function
@@ -175,47 +146,44 @@ class Salsa20Engine extends BaseStreamCipher {
     x.setAll( 0, input );
 
     for( var i=rounds ; i>0 ; i-=2 ) {
-      x[ 4] ^= _rotl((x[ 0]+x[12]), 7);
-      x[ 8] ^= _rotl((x[ 4]+x[ 0]), 9);
-      x[12] ^= _rotl((x[ 8]+x[ 4]),13);
-      x[ 0] ^= _rotl((x[12]+x[ 8]),18);
-      x[ 9] ^= _rotl((x[ 5]+x[ 1]), 7);
-      x[13] ^= _rotl((x[ 9]+x[ 5]), 9);
-      x[ 1] ^= _rotl((x[13]+x[ 9]),13);
-      x[ 5] ^= _rotl((x[ 1]+x[13]),18);
-      x[14] ^= _rotl((x[10]+x[ 6]), 7);
-      x[ 2] ^= _rotl((x[14]+x[10]), 9);
-      x[ 6] ^= _rotl((x[ 2]+x[14]),13);
-      x[10] ^= _rotl((x[ 6]+x[ 2]),18);
-      x[ 3] ^= _rotl((x[15]+x[11]), 7);
-      x[ 7] ^= _rotl((x[ 3]+x[15]), 9);
-      x[11] ^= _rotl((x[ 7]+x[ 3]),13);
-      x[15] ^= _rotl((x[11]+x[ 7]),18);
-      x[ 1] ^= _rotl((x[ 0]+x[ 3]), 7);
-      x[ 2] ^= _rotl((x[ 1]+x[ 0]), 9);
-      x[ 3] ^= _rotl((x[ 2]+x[ 1]),13);
-      x[ 0] ^= _rotl((x[ 3]+x[ 2]),18);
-      x[ 6] ^= _rotl((x[ 5]+x[ 4]), 7);
-      x[ 7] ^= _rotl((x[ 6]+x[ 5]), 9);
-      x[ 4] ^= _rotl((x[ 7]+x[ 6]),13);
-      x[ 5] ^= _rotl((x[ 4]+x[ 7]),18);
-      x[11] ^= _rotl((x[10]+x[ 9]), 7);
-      x[ 8] ^= _rotl((x[11]+x[10]), 9);
-      x[ 9] ^= _rotl((x[ 8]+x[11]),13);
-      x[10] ^= _rotl((x[ 9]+x[ 8]),18);
-      x[12] ^= _rotl((x[15]+x[14]), 7);
-      x[13] ^= _rotl((x[12]+x[15]), 9);
-      x[14] ^= _rotl((x[13]+x[12]),13);
-      x[15] ^= _rotl((x[14]+x[13]),18);
+      x[ 4] ^= crotl32((x[ 0]+x[12]), 7);
+      x[ 8] ^= crotl32((x[ 4]+x[ 0]), 9);
+      x[12] ^= crotl32((x[ 8]+x[ 4]),13);
+      x[ 0] ^= crotl32((x[12]+x[ 8]),18);
+      x[ 9] ^= crotl32((x[ 5]+x[ 1]), 7);
+      x[13] ^= crotl32((x[ 9]+x[ 5]), 9);
+      x[ 1] ^= crotl32((x[13]+x[ 9]),13);
+      x[ 5] ^= crotl32((x[ 1]+x[13]),18);
+      x[14] ^= crotl32((x[10]+x[ 6]), 7);
+      x[ 2] ^= crotl32((x[14]+x[10]), 9);
+      x[ 6] ^= crotl32((x[ 2]+x[14]),13);
+      x[10] ^= crotl32((x[ 6]+x[ 2]),18);
+      x[ 3] ^= crotl32((x[15]+x[11]), 7);
+      x[ 7] ^= crotl32((x[ 3]+x[15]), 9);
+      x[11] ^= crotl32((x[ 7]+x[ 3]),13);
+      x[15] ^= crotl32((x[11]+x[ 7]),18);
+      x[ 1] ^= crotl32((x[ 0]+x[ 3]), 7);
+      x[ 2] ^= crotl32((x[ 1]+x[ 0]), 9);
+      x[ 3] ^= crotl32((x[ 2]+x[ 1]),13);
+      x[ 0] ^= crotl32((x[ 3]+x[ 2]),18);
+      x[ 6] ^= crotl32((x[ 5]+x[ 4]), 7);
+      x[ 7] ^= crotl32((x[ 6]+x[ 5]), 9);
+      x[ 4] ^= crotl32((x[ 7]+x[ 6]),13);
+      x[ 5] ^= crotl32((x[ 4]+x[ 7]),18);
+      x[11] ^= crotl32((x[10]+x[ 9]), 7);
+      x[ 8] ^= crotl32((x[11]+x[10]), 9);
+      x[ 9] ^= crotl32((x[ 8]+x[11]),13);
+      x[10] ^= crotl32((x[ 9]+x[ 8]),18);
+      x[12] ^= crotl32((x[15]+x[14]), 7);
+      x[13] ^= crotl32((x[12]+x[15]), 9);
+      x[14] ^= crotl32((x[13]+x[12]),13);
+      x[15] ^= crotl32((x[14]+x[13]),18);
     }
 
     for (int i = 0; i < _STATE_SIZE; ++i) {
-      x[i] += input[i];
+      x[i] = sum32(x[i], input[i]);
     }
   }
-
-  /// Salsa20Core Helper funcion
-  int _rotl(int x, int y)  => (x << y) | (new Uint32(x)>>-y).toInt();
 
 }
 
