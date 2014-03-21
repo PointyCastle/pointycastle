07dedb0e1b02a5d52774af5ab274a5ab74c135bf
Ivan Zaera Avellon <izaera@gmail.com>
2014-03-21 21:54:22 +0100

    Remove command line tool
    It was only used for benchmarks and we already have better benchmarks in place,  so it will be delayed until all project is stable and nothing of more priority exists.

--- bin/src/util.dart
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2013, Iván Zaera Avellón - izaera@gmail.com  
-// Use of this source code is governed by a LGPL v3 license. 
-// See the LICENSE file for more information.
-
-library cipher.bin.src.util;
-
-String arg( List<String> args, int index ) => (index<args.length) ? args[index] : null;
