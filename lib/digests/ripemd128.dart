633b3a50302cf3578453cfd6e476a71263fb8604
Ivan Zaera Avellon <izaera@gmail.com>
2014-10-15 22:51:24 +0200

    [#84] Dually license the project under Mozilla Public License as well as under LGPL

--- lib/digests/ripemd128.dart
+++ lib/digests/ripemd128.dart
@@ -1,6 +1,9 @@
-// Copyright (c) 2013, Iván Zaera Avellón - izaera@gmail.com
-// Use of this source code is governed by a LGPL v3 license.
-// See the LICENSE file for more information.
+// Copyright (c) 2013-present, Iván Zaera Avellón - izaera@gmail.com
+
+// This library is dually licensed under LGPL 3 and MPL 2.0. See file LICENSE for more information.
+
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
+// the MPL was not distributed with this file, you can obtain one at http://mozilla.org/MPL/2.0/.
 
 library cipher.digests.ripemd128;
 
