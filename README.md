41b0c7b1b23613d3f4405c80e416fa57ed367178
Steven Roose <stevenroose@gmail.com>
2018-01-28 18:35:29 +0100

    Update README and add export.dart

--- README.md
+++ README.md
@@ -10,26 +10,20 @@ To make sure nothing fails, tests and benchmarks for every algorithm are
 provided. The expected results are taken from the Bouncy Castle Java version 
 and also from standards, and matched against the results got from Pointy Castle.
 
+# Algorithms 
+
 As of the last release, the following algorithms are implemented:
 
-
 **Block ciphers:**
-
   * AES
 
-
 **Asymmetric block ciphers:**
-
   * RSA
 
-
 **Stream ciphers:**
-
   * Salsa20
 
-
 **Block cipher modes of operation:**
-
   * CBC (Cipher Block Chaining mode)
   * CFB (Cipher Feedback mode)
   * ECB (Electronic Code Book mode)
@@ -38,14 +32,10 @@ As of the last release, the following algorithms are implemented:
   * CTR (Counter mode)
   * SIC
 
-
 **Paddings:**
-
   * PKCS7 
 
-
 **Digests:**
-
   * MD2
   * MD4
   * MD5
@@ -63,34 +53,110 @@ As of the last release, the following algorithms are implemented:
   * Tiger
   * Whirlpool
 
-
 **MACs:**
-
   * HMAC
-  
-  
-**Signatures:**
 
+**Signatures:**
   * (DET-)ECDSA
   * RSA
-  
-  
-**Password based key derivators:**
 
+**Password based key derivators:**
   * PBKDF2
   * scrypt
-  
-  
-**Asymmetric key generators:**
 
+**Asymmetric key generators:**
   * ECDSA
   * RSA
 
-
 **Secure PRNGs:**
-
   * Based on block cipher in CTR mode
   * Based on block cipher in CTR mode with auto reseed (for forward security)
   * Based on Fortuna algorithm
+
+
+# Usage
+
+There are two ways to use the algorithms that PointyCastle provides: with or 
+without using the registry.
+
+## Registry
+
+The registry allows users to easily instantiate classes for the algorithms using
+the algorithm shorthands like given in the list above.  It also makes it possible
+to seamlessly chain different algorithms together.  For example:
+
+```dart
+import "package:pointycastle/pointycastly.dart";
+
+void main() {
+  Digest sha256 = new Digest("SHA-256");
+  // or
+  KeyDerivator derivator = new KeyDerivator("SHA-1/HMAC/PBKDF2");
+}
+```
+
+## Without the registry
+
+Using the registry means that all algorithms will be imported by default, which
+can possibly increase the compiled size of your program.  To avoid this, it is 
+possible to import algorithms one by one.  In that case, you can decide to either
+use the classes directly, or still use the registry.  But remember that the 
+registry only contains the classes that you import.  For example:
+
+```dart
+import "package:pointycastle/api.dart";
+
+import "package:pointycastle/digests/sha256.dart";
+
+import "package:pointycastle/digests/sha1.dart";
+import "package:pointycastle/macs/hmac.dart";
+import "package:pointycastle/key_derivators/pbkdf2.dart";
+
+void main() {
+  Digest sha256 = new SHA256Digest();
+  // or
+  KeyDerivator derivator = new PBKDF2KeyDerivator(
+      new HMac(new SHA1Digest(), 64));
   
-  
+  // But the registry keeps working for all imported algorithms:
+
+  Digest sha256 = new Digest("SHA-256");
+  // or
+  KeyDerivator derivator = new KeyDerivator("SHA-1/HMAC/PBKDF2");
+}
+```
+
+## Without dart:mirrors or package:reflectable (Flutter)
+
+Because the registry uses reflectable to register all imported algorithm classes
+and reflectable imports `dart:mirrors`, using the registry depends on the ability
+to import mirrors.  Since the [Flutter](https://flutter.io) frame work 
+[doesn't allow using dart:mirrors](https://github.com/flutter/flutter/issues/1150),
+it's not possible to use the registry with Flutter.
+
+The way to use Pointy Castle in Flutter is the way explained in the previous 
+section.  However, there is a utility library that exports all available 
+algorithms at once.
+
+```dart
+import "package:pointycastle/export.dart";
+
+void main() {
+  Digest sha256 = new SHA256Digest();
+  // or
+  KeyDerivator derivator = new PBKDF2KeyDerivator(
+      new HMac(new SHA1Digest(), 64));
+}
+```
+
+
+## Libraries
+
+ * `package:pointycastle/pointycastle.dart`: exports the high-level API and the 
+    registry loaded with all available implementations
+ * `package:pointycastle/api.dart`: exports the high-level API and the registry 
+    without any implementations
+ * `package:pointycastle/export.dart`: exports the API and all implementation 
+    classes
+
+
--- lib/impl.dart
+++ lib/impl.dart
@@ -10,121 +10,18 @@ library pointycastle.impl;
 
 
 // cipher implementations
-import "package:pointycastle/adapters/stream_cipher_as_block_cipher.dart";
 
 // asymmetric
 export "package:pointycastle/asymmetric/api.dart";
-import "package:pointycastle/asymmetric/pkcs1.dart";
-import "package:pointycastle/asymmetric/rsa.dart";
 
-// block
-import "package:pointycastle/block/aes_fast.dart";
-// block/modes
-import "package:pointycastle/block/modes/cbc.dart";
-import "package:pointycastle/block/modes/cfb.dart";
-import "package:pointycastle/block/modes/ctr.dart";
-import "package:pointycastle/block/modes/ecb.dart";
-import "package:pointycastle/block/modes/gctr.dart";
-import "package:pointycastle/block/modes/ofb.dart";
-import "package:pointycastle/block/modes/sic.dart";
-
-// digests
-import "package:pointycastle/digests/md2.dart";
-import "package:pointycastle/digests/md4.dart";
-import "package:pointycastle/digests/md5.dart";
-import "package:pointycastle/digests/ripemd128.dart";
-import "package:pointycastle/digests/ripemd160.dart";
-import "package:pointycastle/digests/ripemd256.dart";
-import "package:pointycastle/digests/ripemd320.dart";
-import "package:pointycastle/digests/sha1.dart";
-import "package:pointycastle/digests/sha224.dart";
-import "package:pointycastle/digests/sha256.dart";
-import "package:pointycastle/digests/sha3.dart";
-import "package:pointycastle/digests/sha384.dart";
-import "package:pointycastle/digests/sha512.dart";
-import "package:pointycastle/digests/sha512t.dart";
-import "package:pointycastle/digests/tiger.dart";
-import "package:pointycastle/digests/whirlpool.dart";
+// This one imports all libraries.
+import "package:pointycastle/export.dart";
 
 // ecc
 export "package:pointycastle/ecc/api.dart";
-import "package:pointycastle/ecc/api.dart";
-import "package:pointycastle/ecc/ecc_base.dart";
-import "package:pointycastle/ecc/ecc_fp.dart" as fp;
 
 // key_derivators
 export "package:pointycastle/key_derivators/api.dart";
-import "package:pointycastle/key_derivators/api.dart";
-import "package:pointycastle/key_derivators/pbkdf2.dart";
-import "package:pointycastle/key_derivators/scrypt.dart";
 
 // key_generators
 export "package:pointycastle/key_generators/api.dart";
-import "package:pointycastle/key_generators/api.dart";
-import "package:pointycastle/key_generators/ec_key_generator.dart";
-import "package:pointycastle/key_generators/rsa_key_generator.dart";
-
-// macs
-import "package:pointycastle/macs/hmac.dart";
-
-// paddings
-import "package:pointycastle/padded_block_cipher/padded_block_cipher_impl.dart";
-import "package:pointycastle/paddings/pkcs7.dart";
-
-// random
-import "package:pointycastle/random/auto_seed_block_ctr_random.dart";
-import "package:pointycastle/random/block_ctr_random.dart";
-import "package:pointycastle/random/fortuna_random.dart";
-
-// signers
-import "package:pointycastle/signers/ecdsa_signer.dart";
-import "package:pointycastle/signers/rsa_signer.dart";
-
-// stream
-import "package:pointycastle/stream/ctr.dart";
-import "package:pointycastle/stream/salsa20.dart";
-import "package:pointycastle/stream/sic.dart";
-
-// ecc curves
-import "package:pointycastle/ecc/curves/brainpoolp160r1.dart";
-import "package:pointycastle/ecc/curves/brainpoolp160t1.dart";
-import "package:pointycastle/ecc/curves/brainpoolp192r1.dart";
-import "package:pointycastle/ecc/curves/brainpoolp192t1.dart";
-import "package:pointycastle/ecc/curves/brainpoolp224r1.dart";
-import "package:pointycastle/ecc/curves/brainpoolp224t1.dart";
-import "package:pointycastle/ecc/curves/brainpoolp256r1.dart";
-import "package:pointycastle/ecc/curves/brainpoolp256t1.dart";
-import "package:pointycastle/ecc/curves/brainpoolp320r1.dart";
-import "package:pointycastle/ecc/curves/brainpoolp320t1.dart";
-import "package:pointycastle/ecc/curves/brainpoolp384r1.dart";
-import "package:pointycastle/ecc/curves/brainpoolp384t1.dart";
-import "package:pointycastle/ecc/curves/brainpoolp512r1.dart";
-import "package:pointycastle/ecc/curves/brainpoolp512t1.dart";
-import "package:pointycastle/ecc/curves/gostr3410_2001_cryptopro_a.dart";
-import "package:pointycastle/ecc/curves/gostr3410_2001_cryptopro_b.dart";
-import "package:pointycastle/ecc/curves/gostr3410_2001_cryptopro_c.dart";
-import "package:pointycastle/ecc/curves/gostr3410_2001_cryptopro_xcha.dart";
-import "package:pointycastle/ecc/curves/gostr3410_2001_cryptopro_xchb.dart";
-import "package:pointycastle/ecc/curves/prime192v1.dart";
-import "package:pointycastle/ecc/curves/prime192v2.dart";
-import "package:pointycastle/ecc/curves/prime192v3.dart";
-import "package:pointycastle/ecc/curves/prime239v1.dart";
-import "package:pointycastle/ecc/curves/prime239v2.dart";
-import "package:pointycastle/ecc/curves/prime239v3.dart";
-import "package:pointycastle/ecc/curves/prime256v1.dart";
-import "package:pointycastle/ecc/curves/secp112r1.dart";
-import "package:pointycastle/ecc/curves/secp112r2.dart";
-import "package:pointycastle/ecc/curves/secp128r1.dart";
-import "package:pointycastle/ecc/curves/secp128r2.dart";
-import "package:pointycastle/ecc/curves/secp160k1.dart";
-import "package:pointycastle/ecc/curves/secp160r1.dart";
-import "package:pointycastle/ecc/curves/secp160r2.dart";
-import "package:pointycastle/ecc/curves/secp192k1.dart";
-import "package:pointycastle/ecc/curves/secp192r1.dart";
-import "package:pointycastle/ecc/curves/secp224k1.dart";
-import "package:pointycastle/ecc/curves/secp224r1.dart";
-import "package:pointycastle/ecc/curves/secp256k1.dart";
-import "package:pointycastle/ecc/curves/secp256r1.dart";
-import "package:pointycastle/ecc/curves/secp384r1.dart";
-import "package:pointycastle/ecc/curves/secp521r1.dart";
-
