1bb6352459effe0c3962e95669d853f905bebf3c
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2018-08-07 16:53:44 +0900

    fix benchmark

--- benchmark/all_benchmarks.dart
+++ benchmark/all_benchmarks.dart
@@ -4,10 +4,9 @@
 
 library pointycastle.benchmark.all_benchmarks;
 
-import "./src/ufixnum_benchmark.dart" as ufixnum_benchmark;
+import 'package:pointycastle/pointycastle.dart';
 
 import "./block/aes_fast_benchmark.dart" as aes_fast_benchmark;
-
 import "./digests/md2_benchmark.dart" as md2_benchmark;
 import "./digests/md4_benchmark.dart" as md4_benchmark;
 import "./digests/md5_benchmark.dart" as md5_benchmark;
@@ -18,16 +17,17 @@ import "./digests/ripemd320_benchmark.dart" as ripemd320_benchmark;
 import "./digests/sha1_benchmark.dart" as sha1_benchmark;
 import "./digests/sha224_benchmark.dart" as sha224_benchmark;
 import "./digests/sha256_benchmark.dart" as sha256_benchmark;
-import "./digests/sha3_benchmark.dart" as sha3_benchmark;
 import "./digests/sha384_benchmark.dart" as sha384_benchmark;
+import "./digests/sha3_benchmark.dart" as sha3_benchmark;
 import "./digests/sha512_benchmark.dart" as sha512_benchmark;
 import "./digests/sha512t_benchmark.dart" as sha512t_benchmark;
 import "./digests/tiger_benchmark.dart" as tiger_benchmark;
 import "./digests/whirlpool_benchmark.dart" as whirlpool_benchmark;
-
+import "./src/ufixnum_benchmark.dart" as ufixnum_benchmark;
 import "./stream/salsa20_benchmark.dart" as salsa20_benchmark;
 
 void main() {
+  initCipher();
 
   // api
   ufixnum_benchmark.main();
@@ -55,5 +55,4 @@ void main() {
 
   // stream ciphers
   salsa20_benchmark.main();
-
 }
