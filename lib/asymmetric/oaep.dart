0724f3077b26ab26c940623758e0393b12ccb3dd
Duncan <duncan.hoggan@nologostudios.com>
2018-11-22 09:15:15 +0200

    A few formatting updates

--- lib/asymmetric/oaep.dart
+++ lib/asymmetric/oaep.dart
@@ -24,8 +24,6 @@ class OAEPEncoding extends BaseAsymmetricBlockCipher {
             return new OAEPEncoding(underlyingCipher);
           });
 
-
-
   Digest hash = SHA1Digest();
   Digest mgf1Hash;
   Uint8List defHash = Uint8List(SHA1Digest().digestSize);
@@ -49,25 +47,19 @@ class OAEPEncoding extends BaseAsymmetricBlockCipher {
     return new Uint8List.fromList(seeds);
   }
 
-
-
-// for compat cleaner translation from java source
-Uint8List _arraycopy(Uint8List src, int srcPos, Uint8List dest, int destPos, int length) {
-// setRange(int start, int end, Iterable<int> iterable, [ int skipCount = 0 ]) → void
-// Copies the objects of iterable, skipping skipCount objects first, into the range start, inclusive, to end, exclusive, of the list. [...]
-
-    dest.setRange(destPos, destPos + length , src.sublist(srcPos, srcPos + length));
+  // for compat cleaner translation from java source
+  Uint8List _arraycopy(
+      Uint8List src, int srcPos, Uint8List dest, int destPos, int length) {
+    dest.setRange(
+        destPos, destPos + length, src.sublist(srcPos, srcPos + length));
     return dest;
   }
 
-// check
   void init(bool forEncryption, CipherParameters params) {
     AsymmetricKeyParameter akparams;
     mgf1Hash = hash;
-
     if (params is ParametersWithRandom) {
       ParametersWithRandom paramswr = params;
-
       _random = paramswr.random;
       akparams = paramswr.parameters;
     } else {
@@ -75,16 +67,12 @@ Uint8List _arraycopy(Uint8List src, int srcPos, Uint8List dest, int destPos, int
       _random.seed(KeyParameter(_seed()));
       akparams = params;
     }
-
     _engine.init(forEncryption, akparams);
-
     _forEncryption = forEncryption;
   }
 
-
   int get inputBlockSize {
     var baseBlockSize = _engine.inputBlockSize;
-
     if (_forEncryption) {
       return baseBlockSize - 1 - 2 * defHash.length;
     } else {
@@ -94,7 +82,6 @@ Uint8List _arraycopy(Uint8List src, int srcPos, Uint8List dest, int destPos, int
 
   int get outputBlockSize {
     var baseBlockSize = _engine.outputBlockSize;
-
     if (_forEncryption) {
       return baseBlockSize;
     } else {
@@ -102,15 +89,14 @@ Uint8List _arraycopy(Uint8List src, int srcPos, Uint8List dest, int destPos, int
     }
   }
 
-  int processBlock(Uint8List inp, int inpOff, int len, Uint8List out, int outOff) {
+  int processBlock(
+      Uint8List inp, int inpOff, int len, Uint8List out, int outOff) {
     if (_forEncryption) {
       return _encodeBlock(inp, inpOff, len, out, outOff);
     } else {
       return _decodeBlock(inp, inpOff, len, out, outOff);
     }
   }
-
-  // ---------------------------PROG----------------------
 
   int _encodeBlock(
       Uint8List inp, int inpOff, int inpLen, Uint8List out, int outOff) {
@@ -124,7 +110,6 @@ Uint8List _arraycopy(Uint8List src, int srcPos, Uint8List dest, int destPos, int
     // copy in the message
     //
     // block.setRange(inpOff, block.length - inpLen, inp.sublist(inpLen));
-    
     block = _arraycopy(inp, inpOff, block, block.length - inpLen, inpLen);
 
     //
@@ -150,7 +135,6 @@ Uint8List _arraycopy(Uint8List src, int srcPos, Uint8List dest, int destPos, int
     // mask the message block.
     //
     Uint8List mask = _maskGeneratorFunction1(seed, 0, seed.length, block.length - defHash.length);
-
     for (int i = defHash.length; i != block.length; i++) {
       block[i] ^= mask[i - defHash.length];
     }
@@ -200,8 +184,7 @@ Uint8List _arraycopy(Uint8List src, int srcPos, Uint8List dest, int destPos, int
     //
     // unmask the message block.
     //
-    mask = _maskGeneratorFunction1(
-        block, 0, defHash.length, block.length - defHash.length);
+    mask = _maskGeneratorFunction1(block, 0, defHash.length, block.length - defHash.length);
 
     for (int i = defHash.length; i != block.length; i++) {
       block[i] ^= mask[i - defHash.length];
@@ -244,15 +227,14 @@ Uint8List _arraycopy(Uint8List src, int srcPos, Uint8List dest, int destPos, int
     output = _arraycopy(block, start, output, 0, output.length);
 
     var result = new Uint8List(block.length - start);
-
     var rlen = (block.length - start);
     out.setRange(0, rlen, block.sublist(start));
     return rlen;
   }
 
-  /**
-     * int to octet string.
-     */
+ /**
+  * int to octet string.
+  */
   Uint8List _ItoOSP(int i, Uint8List sp) {
     sp[0] = i >> 24;
     sp[1] = i >> 16;
@@ -263,38 +245,30 @@ Uint8List _arraycopy(Uint8List src, int srcPos, Uint8List dest, int destPos, int
 
 /**
  * mask generator function, as described in PKCS1v2.
- * 
- * NEED TESTS!!!
  */
-  Uint8List _maskGeneratorFunction1(
-      Uint8List Z, int zOff, int zLen, int length) {
+  Uint8List _maskGeneratorFunction1(Uint8List Z, int zOff, int zLen, int length) {
     Uint8List mask = Uint8List(length);
     Uint8List hashBuf = Uint8List(mgf1Hash.digestSize);
     Uint8List C = Uint8List(4);
     int counter = 0;
-
     mgf1Hash.reset();
 
     while (counter < (length / hashBuf.length)) {
       _ItoOSP(counter, C);
-
       mgf1Hash.update(Z, zOff, zLen);
       mgf1Hash.update(C, 0, C.length);
       mgf1Hash.doFinal(hashBuf, 0);
-
       hashBuf = _arraycopy(hashBuf, 0, mask, counter * hashBuf.length, hashBuf.length);
-
       counter++;
     }
 
     if ((counter * hashBuf.length) < length) {
       _ItoOSP(counter, C);
-
       mgf1Hash.update(Z, zOff, zLen);
       mgf1Hash.update(C, 0, C.length);
       mgf1Hash.doFinal(hashBuf, 0);
-
-      hashBuf = _arraycopy(hashBuf, 0, mask, counter * hashBuf.length,mask.length - (counter * hashBuf.length));
+      hashBuf = _arraycopy(hashBuf, 0, mask, counter * hashBuf.length,
+          mask.length - (counter * hashBuf.length));
     }
     return mask;
   }
