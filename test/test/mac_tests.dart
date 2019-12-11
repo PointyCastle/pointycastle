e3268988f0aee867552f2f3c9e985ac414c2b006
Hoylen Sue <hoylen@hoylen.com>
2019-12-12 08:05:21 +1000

    Added HMAC tests cases for SHA-224/HMAC, 256, 384, 512 from RFC 4231.

--- test/test/mac_tests.dart
+++ test/test/mac_tests.dart
@@ -40,3 +40,57 @@ void _runMacTest(Mac mac, Uint8List plainText, String expectedHexDigestText) {
 
   expect(hexOut, equals(expectedHexDigestText));
 }
+
+/// HMAC tests with test vectors from a single test case in
+/// [RFC 4231](https://tools.ietf.org/html/rfc4231) _Identifiers and Test
+/// Vectors for HMAC-SHA-224, HMAC-SHA-256, HMAC-SHA-384, and HMAC-SHA-512_.
+
+class Rfc4231TestVector {
+  Rfc4231TestVector(this.name, this.keyHex, this.dataHex, this.hmacSha224,
+      this.hmacSha256, this.hmacSha384, this.hmacSha512,
+      {this.truncate128: false});
+
+  final String name;
+  final String keyHex;
+  final String dataHex;
+  final String hmacSha224;
+  final String hmacSha256;
+  final String hmacSha384;
+  final String hmacSha512;
+  final bool truncate128;
+
+  void run() {
+    final key = createUint8ListFromHexString(keyHex);
+    final data = createUint8ListFromHexString(dataHex);
+
+    group(name, () {
+      // For the blockLengths to use, see _DIGEST_BLOCK_LENGTH in the HMAC class
+      _hmacTest("SHA-224/HMAC", key, data, hmacSha224, truncate128);
+      _hmacTest("SHA-256/HMAC", key, data, hmacSha256, truncate128);
+      _hmacTest("SHA-384/HMAC", key, data, hmacSha384, truncate128);
+      _hmacTest("SHA-512/HMAC", key, data, hmacSha512, truncate128);
+    });
+  }
+
+  void _hmacTest(String hmacName, Uint8List key, Uint8List data,
+      String expected, bool truncate128) {
+    test(hmacName, () {
+      final hmac = Mac(hmacName)..init(KeyParameter(key));
+
+      final d = hmac.process(data);
+
+      final result =
+          formatBytesAsHexString(((truncate128)) ? d.sublist(0, 16) : d);
+      //print('$testName: $result');
+      expect(result, equals(expected));
+    });
+  }
+
+  static void runAll(Iterable<Rfc4231TestVector> testCases) {
+    group("Tests from RFC 4231", () {
+      for (final t in testCases) {
+        t.run();
+      }
+    });
+  }
+}
